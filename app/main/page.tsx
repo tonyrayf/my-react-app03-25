9e3b94fd94527c7c6a9a572e055baf03b46977e1
TonyRayF <wintryshades@gmail.com>
2025-03-23 18:57:19 +0900

    lol

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
 import UserCard from "../components/UserCard";
-import styles from "../styles/UserCard.module.css";
+import styles from "../styles/User.module.css";
 
 export default function Home() {
   return (
