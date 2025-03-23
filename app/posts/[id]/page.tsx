4789fd8046fa848a882b05f7d6498815ed58561b
TonyRayF <wintryshades@gmail.com>
2025-03-23 19:08:20 +0900

    lolol

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,12 +1,11 @@
-export default async function Post({params}: {params: {id: string}}) {
-    const postId = params.id;
-    const response = await fetch(`http://localhost:3000/api/posts/${postId}`);
+export default async function Post({ params }: { params: { id: string } }) {
+    const response = await fetch(`http://localhost:3000/api/posts/${params.id}`);
     const post = await response.json();
-
-    return(
-        <div>
-            <h1>{post.title}</h1>
-            <p>{post.content}</p>
-        </div>
+  
+    return (
+    <div>
+        <h1>{post.title}</h1>
+        <p>{post.content}</p>
+    </div>
     );
-}
+  }
